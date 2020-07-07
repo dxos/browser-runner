bd8d2f6cc580e377dd77e4d2884116b3ec4c58f4
Dmytro Maretskyi <maretskii@gmail.com>
2020-07-07 11:41:47 +0300

    Lint

--- src/runtime.js
+++ src/runtime.js
@@ -1,12 +1,12 @@
-if(!window.process) {
-  window.process = {}
+if (!window.process) {
+  window.process = {};
 }
 
 window.process.exit = (code = 0) => {
-  window.exit = code
-}
+  window.exit = code;
+};
 
-const EventEmitter = require('events')
+const EventEmitter = require('events');
 
 const emitter = new EventEmitter();
 
@@ -14,8 +14,8 @@ window.process.on = (...args) => emitter.on(...args);
 
 window.process.send = msg => {
   window.__ipcSend(msg);
-}
+};
 
 window.__ipcReceive = data => {
   emitter.emit('message', data.type === 'Buffer' ? Buffer.from(data) : data);
-}
+};
